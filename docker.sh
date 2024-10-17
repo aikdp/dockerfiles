4cb8fb26dec5ea1fe3912eb77dbcdb0ea67d254c
aikdp <kdprasad028@gmail.com>
2024-10-17 10:59:22 +0530

    docker practice

--- docker.sh
+++ docker.sh
@@ -8,7 +8,7 @@
 #     exit 1
 # fi
 
-sudo su -
+
 # sudo yum install -y yum-utils
 # sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 # sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
